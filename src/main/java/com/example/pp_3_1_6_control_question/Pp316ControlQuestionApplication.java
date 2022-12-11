1dbb01163c92eca91cbc4fe4864c9dd6605eda94
Eko <ekoslambek@gmail.com>
2022-12-11 14:53:15 +0600

    1st

--- src/main/java/com/example/pp_3_1_6_control_question/Pp316ControlQuestionApplication.java
+++ src/main/java/com/example/pp_3_1_6_control_question/Pp316ControlQuestionApplication.java
@@ -1,13 +1,23 @@
 package com.example.pp_3_1_6_control_question;
 
+import com.example.pp_3_1_6_control_question.config.MyConfig;
+import com.example.pp_3_1_6_control_question.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Pp316ControlQuestionApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(Pp316ControlQuestionApplication.class, args);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyConfig.class);
+        HTTPMethods httpMethods = applicationContext.getBean("HTTPMethods", HTTPMethods.class);
+        List<String> allUsers = httpMethods.getAllUsers();
+        System.out.println(allUsers);
+        httpMethods.saveUser(new User(3L, "James", "Brown", (byte) 15));
+        httpMethods.updateUser(new User(3L, "Thomas", "Shelby", (byte) 30));
+        httpMethods.deleteUser(3L);
+        System.out.println(HTTPMethods.result);
     }
-
 }
--- src/main/java/com/example/pp_3_1_6_control_question/config/MyConfig.java
+++ src/main/java/com/example/pp_3_1_6_control_question/config/MyConfig.java
@@ -1,2 +1,25 @@
-package com.example.pp_3_1_6_control_question.config;public class MyConfig {
+package com.example.pp_3_1_6_control_question.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@ComponentScan("com.example.pp_3_1_6_control_question")
+public class MyConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
+    }
 }
